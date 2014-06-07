bb6a79455d6c1895c5a88708d3c19e015916434a
Torsten Becker <torsten.becker@gmail.com>
2014-06-06 23:50:26 -0400

    Test against Clojure 1.5 and 1.6.

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
 (defproject zookeeper-atom "0.1.0"
   :description "Provides a reference type that looks and behaves like a Clojure atom but uses Zookeeper to store and synchronize distributed access to it's value."
   :url "https://github.com/torsten/zookeeper-atom"
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.3.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [zookeeper-clj "0.9.1"]
                  [org.clojure/tools.logging "0.2.6"]]
   :main zookeeper-atom
-  :aliases {"test" ["midje"]}
-  :profiles
-  {:dev {:dependencies [[midje "1.6.3"]
-                        [clj-logging-config "1.9.10"]]
-         :plugins [[lein-midje "3.0.0"]]}}
-)
+  :aliases {"test" ["midje"]
+            "all" ["with-profile" "dev,1.6:dev,1.5"]}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :dev {:dependencies [[midje "1.6.3"]
+                                  [clj-logging-config "1.9.10"]]
+                   :plugins [[lein-midje "3.0.0"]]}})
